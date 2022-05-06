31cea97e711f929c6f34486ff5b9557579cc987c
gbz45948 <ben.gaunt@rfi.ac.uk>
2022-05-06 14:53:37 +0000

    Added to import_testing.py
    More lines of code unpicking what exactly is in the .pts file, scouring through it byte by byte to try and find where the actual data starts.

--- import_testing.py
+++ import_testing.py
@@ -28,9 +28,58 @@ for intkey in intkeys:
     data_i = view_four_data[str(intkey)]
     print(str(intkey), data_i['Keyword'], data_i['Filename'])
 
+img_fname = path + view_four_data['0']['Filename']
 pts_fname = path + view_four_data['5']['Filename']
 
-with open(pts_fname, 'br') as fn:
-    text_junk = fn.read()
 
-print(text_junk)
+"""
+streamy = BytesIO()
+with open(pts_fname, 'br') as fn:
+    streamy.write(fn.read())
+streamy.seek(0)
+fm_bytes = streamy.read(4)
+print(fm_bytes)
+print(calcsize('<I'))
+file_magic_arr = unpack('<I', fm_bytes)
+print(file_magic_arr)
+file_magic = file_magic_arr[0]
+print(file_magic)
+print('reading PTS')
+
+
+ff_bytes = streamy.read(8)
+print(ff_bytes)
+
+streamy.seek(164)
+#more_hex = streamy.read(800)
+#print(more_hex)
+
+dict_of_hex = aggregate(streamy)
+print(dict_of_hex)
+"""
+
+
+
+streamy = BytesIO()
+with open(img_fname, 'br') as fn:
+    streamy.write(fn.read())
+streamy.seek(0)
+file_magic = unpack('<I', streamy.read(4))[0]
+print('reading IMG')
+
+img_fileformat = streamy.read(32).rstrip(b'\x00').decode("utf-8")
+header, header_len, data = unpack('<III', streamy.read(12))
+print(header, header_len, data)
+streamy.seek(header)
+print(streamy.read(12))
+header = aggregate(streamy)
+streamy.seek(data)
+print(streamy.read(12))
+metadata = aggregate(streamy)
+s = metadata['Image']['Size']
+metadata['Image']['Bits'].resize((s[1], s[0]))
+image_array = metadata['Image']['Bits']
+
+print(header)
+
+
